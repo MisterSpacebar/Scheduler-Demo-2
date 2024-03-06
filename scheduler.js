31ba89594a3b59897c7c7b8439c4db1874a30897
MisterSpacebar <38817617+MisterSpacebar@users.noreply.github.com>
2024-03-06 13:24:43 -0500

    range bars are now working and in-sync with time input fields

--- scheduler.js
+++ scheduler.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
 
     function minutesToTime(minutes) {
         let hours = Math.floor(minutes / 60);
+        if (hours < 10){
+            hours = '0' + hours;
+        }
         let mins = minutes % 60;
         return `${hours}:${mins.toString().padStart(2, '0')}`;
     }
@@ -13,30 +16,6 @@ $(document).ready(function() {
         return hours * 60 + minutes;
     }
 
-    // Initialize the noUiSliders for each day
-    const $sliderDouble = document.createElement('div');
-    const $sliderSingle = document.createElement('div');
-    $('.slider-single').append($sliderSingle);
-    $('.slider-double').append($sliderDouble);
-
-    noUiSlider.create($sliderSingle, {
-        start: [450, 1080],
-        connect: true,
-        range: {
-            'min': [240],
-            'max': [1320]
-        },
-    });
-
-    noUiSlider.create($sliderDouble, {
-        start: [450, 700, 800, 1080],
-        connect: [false, true, false, true, false],
-        range: {
-            'min': [240],
-            'max': [1320]
-        },
-    });
-
     days.forEach(day => {
         // labels and time inputs
         const $daySection = $('.timeInputs').clone().show();
@@ -44,18 +23,57 @@ $(document).ready(function() {
         $daySection.find('.btn-check').each(function(index) {
             $(this).attr('id', `${$(this).attr('id')}-${day}`);
             $(this).attr('name', `btnradio-${day}`);
+            $(this).attr('data-day', day);
         });
         $daySection.find('label').each(function(index) {
             const forAttr = $(this).attr('for');
             $(this).attr('for', `${forAttr}-${day}`);
         });
-        $daySection.prepend(`<h3 class="weekday">${day}</h3>`);
+        // time inputs
+        $daySection.find('.slider-single').attr('id', `slider-single-${day}`);
+        $daySection.find('.slider-double').attr('id', `slider-double-${day}`);
+        // day label
+        $daySection.prepend(`<h3 class="weekday" data-day="${day}">${day}</h3>`);
+        let thisDay = $('.day-section').attr('data-day');
 
+        // create time input identity
+        $('.single-table').attr('id',`single-table-${thisDay}`);
+        $('.double-table').attr('id',`double-table-${thisDay}`);
+
+        // Initialize the noUiSliders for each day
+        const $sliderDouble = document.createElement('div');
+        const $sliderSingle = document.createElement('div');
+    
+        noUiSlider.create($sliderSingle, {
+            start: [450, 1080],
+            connect: true,
+            range: {
+                'min': [240],
+                'max': [1440]
+            },
+            step: 15
+        });
+    
+        noUiSlider.create($sliderDouble, {
+            start: [450, 700, 800, 1080],
+            connect: [false, true, false, true, false],
+            range: {
+                'min': [240],
+                'max': [1440]
+            },
+            step: 15
+        });
+
+        // Append the sliders to the day section
+        $daySection.find('.slider-single').append($sliderSingle);
+        $daySection.find('.slider-double').append($sliderDouble);
         $('#daysContainer').append($daySection);
 
+        // Event listener for slider to update time inputs
         $sliderSingle.noUiSlider.on('update', function (values, handle) {
             let timeFormatValues = values.map(minutesToTime); // Convert each value to time format
             // Assuming two inputs for a single range slider, adjust if you have more for double range
+            console.log(timeFormatValues);
             $daySection.find('#timeInput1').val(timeFormatValues[0]);
             $daySection.find('#timeInput2').val(timeFormatValues[1]);
         });
@@ -63,12 +81,28 @@ $(document).ready(function() {
         $sliderDouble.noUiSlider.on('update', function (values, handle) {
             let timeFormatValues = values.map(minutesToTime); // Convert each value to time format
             // Assuming two inputs for a single range slider, adjust if you have more for double range
+            console.log(timeFormatValues);
             $daySection.find('#timeInput3').val(timeFormatValues[0]);
             $daySection.find('#timeInput4').val(timeFormatValues[1]);
             $daySection.find('#timeInput5').val(timeFormatValues[2]);
             $daySection.find('#timeInput6').val(timeFormatValues[3]);
         });
 
+        function sliderSingleTimeUpdate() {
+            let startMinutes = timeToMinutes($daySection.find('#timeInput1').val());
+            let endMinutes = timeToMinutes($daySection.find('#timeInput2').val());
+            return [startMinutes, endMinutes];
+        }
+
+        function sliderDoubleTimeUpdate() {
+            let startMinutesOne = timeToMinutes($daySection.find('#timeInput3').val());
+            let endMinutesOne = timeToMinutes($daySection.find('#timeInput4').val());
+            let startMinutesTwo = timeToMinutes($daySection.find('#timeInput5').val());
+            let endMinutesTwo = timeToMinutes($daySection.find('#timeInput6').val());
+            return [startMinutesOne, endMinutesOne, startMinutesTwo, endMinutesTwo];
+        }
+
+        // Event listener for time inputs to update slider
         $daySection.find('.start-time-input, .end-time-input').on('change', function () {
             let startMinutes = timeToMinutes($daySection.find('#timeInput1').val());
             let endMinutes = timeToMinutes($daySection.find('#timeInput2').val());
@@ -83,6 +117,7 @@ $(document).ready(function() {
             slider.noUiSlider.set([startMinutesOne, endMinutesOne, startMinutesTwo, endMinutesTwo]);
         });
 
+        // Hide time inputs and labels
         $daySection.find('input[type="time"]').hide();
         $daySection.find('.time-input-label').hide();
         $('.slider-single').hide();
@@ -93,6 +128,7 @@ $(document).ready(function() {
             const selectedOption = $(this).siblings('label[for="' + $(this).attr('id') + '"]').text();
             switch (selectedOption) {
                 case 'None':
+                    console.log(thisDay + " radio button is selected (NONE)");
                     $daySection.find('input[type="time"]').hide();
                     $daySection.find('.time-input-label').hide();
 
@@ -100,29 +136,29 @@ $(document).ready(function() {
                     $('.slider-double').hide();
                     break;
                 case 'Single':
+                    console.log(thisDay + " radio button is selected (ONE)");
                     $daySection.find('input[type="time"]').hide();
                     $daySection.find('.time-input-label').hide();
                     $daySection.find('#timeInput1, #timeInput2, #start-time-label, #end-time-label').show();
-                    $('.slider-single').hide();
-                    $('.slider-double').hide();
 
-                    $('.slider-single').show();
+                    $(`#slider-single-${day}`).show();
+                    $(`#slider-double-${day}`).hide();
                     $sliderSingle.noUiSlider.updateOptions({
-                        start: [480, 1280],
+                        start: sliderSingleTimeUpdate(),
                         connect: [true],
                     }, true);
                     break;
                 case 'Double':
+                    console.log(thisDay + " radio button is selected (TWO)");
                     $daySection.find('input[type="time"]').hide();
                     $daySection.find('.time-input-label').hide();
                     $daySection.find('#timeInput3, #timeInput4, #timeInput5, #timeInput6').show();
                     $daySection.find('#start-time-1-label, #start-time-2-label, #end-time-1-label, #end-time-2-label').show();
-                    $('.slider-single').hide();
-                    $('.slider-double').hide();
 
-                    $('.slider-double').show();
+                    $(`#slider-double-${day}`).show();
+                    $(`#slider-single-${day}`).hide();
                     $sliderDouble.noUiSlider.updateOptions({
-                        start: [480, 720, 780, 1280],
+                        start: sliderDoubleTimeUpdate(),
                         connect: [true, true, true, false],
                     }, true);
                     break;
@@ -130,4 +166,5 @@ $(document).ready(function() {
         });
 
     });
+
 });
